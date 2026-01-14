eb0a02d0be2af39e16c1320b15e0edf28b04c774
JoaoManuel2005 <johnyka2005@gmail.com>
2026-01-14 22:20:29 +0000

    create microsoft ouath app

---
+++ auth-app/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Header from '../components/Header/header';
+
+export default function IndexPage() {
+  return (
+    <>
+      <Head>
+        <title>Sign In</title>
+      </Head>
+      <Header />
+    </>
+  );
+}
